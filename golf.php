c448431606709fba351c6bc59a76d5d67f3385a1
urthbound <jeffowler@gmail.com>
2015-06-03 21:44:57 -0400

    woah nelly

--- golf.php
+++ golf.php
@@ -1,1 +1,8 @@
-<?while($argv[2]--)@$o+=$argv[1]+=$argv[3]*@$i++;echo"$o\n";
+<?
+$t = 'while($argv[2]--)@$o+=$argv[1]+=$argv[3]*@$i++;echo"$o\n";';
+echo gzencode($t);
+// echo eval(gzdecode(gzencode($t)));
+echo gzdecode('+I,J/?6?P??ca,-?Lmm%??<%k3:%');
+
+$filter     = new Zend\Filter\Compress('Bz2');
+$compressed = $filter->filter('Uncompressed string');
--- golf3.php
+++ golf3.php
@@ -1,2 +1,5 @@
-<?$a='$argv[';$x="echo{$a}1]*{$a}2]+(((--{$a}2]*{$a}3])*({$a}2]+1)*({$a}2]+2))/6).\"\n\";";eval($x);
-
+<?
+$t = '"echo{$a}1]*{$a}2]+(((--{$a}2]*{$a}3])*({$a}2]+1)*({$a}2]+2))/6).\"\n\"";';
+$a='$argv[';
+echo gzuncompress(gzcompress($t));
+// echo eval(gzuncompress(gzcompress($t)));
