fe1bf33140de298b2073f2dccd5ba6910840681b
Jeff Fowler <jeffowler@gmail.com>
2015-06-05 11:44:31 -0400

    all tries work

--- golf.php
+++ golf.php
@@ -1,8 +1,1 @@
-<?
-$t = 'while($argv[2]--)@$o+=$argv[1]+=$argv[3]*@$i++;echo"$o\n";';
-echo gzencode($t);
-// echo eval(gzdecode(gzencode($t)));
-echo gzdecode('+I,J/?6?P??ca,-?Lmm%??<%k3:%');
-
-$filter     = new Zend\Filter\Compress('Bz2');
-$compressed = $filter->filter('Uncompressed string');
+<?while($argv[2]--)@$o+=$argv[1]+=$argv[3]*@$i++;echo"$o\n";
--- golf3.php
+++ golf3.php
@@ -1,5 +1,2 @@
-<?
-$t = '"echo{$a}1]*{$a}2]+(((--{$a}2]*{$a}3])*({$a}2]+1)*({$a}2]+2))/6).\"\n\"";';
-$a='$argv[';
-echo gzuncompress(gzcompress($t));
-// echo eval(gzuncompress(gzcompress($t)));
+<?$a='$argv[';echo eval("echo{$a}1]*{$a}2]+(((--{$a}2]*{$a}3])*({$a}2]+1)*({$a}2]+2))/6).'
+';");
